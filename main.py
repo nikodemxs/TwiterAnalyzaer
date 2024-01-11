3bbd24701b4e0fc6fa43e16fa29f935469a9fadd
Jakub Maślanka <57420365+JakubMaslanka@users.noreply.github.com>
2024-01-11 20:54:02 +0100

    add another console log

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+    print(f'Hello world from JM')  # Press Ctrl+F8 to toggle the breakpoint.
 
 twiterr_account = input(f'Podaj konto twitter')
 
