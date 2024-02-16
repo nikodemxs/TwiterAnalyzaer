8f457e19345a4797491c92ad567788dc0ada11c2
JakubMaslanka <kuba.ath51@gmail.com>
2024-02-16 17:35:29 +0100

    chore: fix export mechanism; utilise logger

--- src/utils/export.py
+++ src/utils/export.py
@@ -1,24 +1,35 @@
 import csv
+import os
 import json
 import pandas as pd
+from src.utils.logger import Logger
 
-def export_to_xlsx(data, filename="output.xlsx"):
+def export_to_xlsx(data, filename="output"):
+    logger = Logger("File Export service")
+    os.makedirs("outputs", exist_ok=True)
+    file_path = f"outputs/{filename}.xlsx"
     df = pd.DataFrame(data)
-    df.to_excel(filename, index=False)
-    print(f"Data exported to {filename} successfully.")
+    df.to_excel(file_path, index=False)
+    logger.info(f"Data exported to {file_path} successfully.")
 
-def export_to_csv(data, filename="output.csv"):
+def export_to_csv(data, filename="output"):
+    logger = Logger("File Export service")
+    os.makedirs("outputs", exist_ok=True)
+    file_path = f"outputs/{filename}.csv"
     keys = data[0].keys() 
-    with open(filename, 'w', newline='') as output_file:
+    with open(file_path, 'w', newline='') as output_file:
         dict_writer = csv.DictWriter(output_file, keys)
         dict_writer.writeheader()
         dict_writer.writerows(data)
-    print(f"Data exported to {filename} successfully.")
+    logger.info(f"Data exported to {file_path} successfully.")
 
-def export_to_json(data, filename="output.json"):
-    with open(filename, 'w') as output_file:
+def export_to_json(data, filename="output"):
+    logger = Logger("File Export service")
+    os.makedirs("outputs", exist_ok=True)
+    file_path = f"outputs/{filename}.json"
+    with open(file_path, 'w') as output_file:
         json.dump(data, output_file)
-    print(f"Data exported to {filename} successfully.")
+    logger.info(f"Data exported to {file_path} successfully.")
 
 def print_in_console(data, filename="output"):
     for item in data:
