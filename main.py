d95ae4a8af2e73806a3d7a95eb4ea8b558db8f1c
JakubMaslanka <kuba.ath51@gmail.com>
2024-02-16 17:36:59 +0100

    feat: utilise logger; integrate Twitter API within service

--- main.py
+++ main.py
@@ -1,66 +1,68 @@
 import os
 import sys
+import time
 import questionary
 from dotenv import load_dotenv
-from src.constants.main import STOCK_API_KEY
-from src.apis.twitter_api import TwitterMockApi
+from src.constants.main import STOCK_API_KEY, TWITTER_BEARER_TOKEN
+from src.apis.twitter_api import TwitterApi
 from src.apis.stock_api import StockAPI
+from src.utils.logger import Logger
 from src.utils.config import load_config, get_ceo_by_config_name
 from src.utils.analysis import analyze_sentiment, analyze_correlation
 from src.utils.date import TimeFrameError, calculate_date_range
 from src.utils.export import export_to_csv, export_to_xlsx, export_to_json, print_in_console
 
 load_dotenv()
+logger = Logger("Script runner")
 config = load_config()
-CEOs = config["availableCEOs"]
+availableCEOs = config["availableCEOs"]
 
 selected_ceo_name = questionary.select(
         "Choose a CEO whose tweets you want to compare to auction data",
-        choices=[ceo["name"] for ceo in CEOs]).ask()
+        choices=[ceo["name"] for ceo in availableCEOs]).ask()
 
-ceo = get_ceo_by_config_name(CEOs, selected_ceo_name)
+ceo = get_ceo_by_config_name(availableCEOs, selected_ceo_name)
 
-twitter_api = TwitterMockApi()
-stock_api = StockAPI(os.getenv(STOCK_API_KEY))
+twitter_api = TwitterApi(bearer_token=os.getenv(TWITTER_BEARER_TOKEN))
+stock_api = StockAPI(api_key=os.getenv(STOCK_API_KEY))
 
-tweets = twitter_api.fetch_user_tweets(ceo["twitterAccountName"], 2)
+tweets = twitter_api.fetch_user_tweets(ceo["twitterAccountName"], 100)
 
 if tweets is None:
-    print("Error: Something went wrong with fetching tweets")
+    logger.error("Something went wrong with fetching tweets")
     sys.exit(1)
 
-# TODO: remove - debugging purposes only
-print(f"Collected tweets: {tweets}")
+logger.debug(f"Collected tweets: {tweets}")
+logger.info(f"Collected {len(tweets)} tweets")
 
-data = []
+output_data = []
 
 for(i, tweet) in enumerate(tweets):
     try:
+        logger.info(f"Analyzing tweet {i+1}/{len(tweets)}")
         sentiment = analyze_sentiment(tweet['text'])
         date_from, date_to = calculate_date_range(tweet['created_at'])
 
-        # TODO: remove - debugging purposes only
-        print(f"Sentiment: {sentiment}")
-        print(f"Date from: {date_from}")
-        print(f"Date to: {date_to}")
+        logger.debug(f"Sentiment: {sentiment}")
+        logger.debug(f"Date from: {date_from}")
+        logger.debug(f"Date to: {date_to}")
 
         stock_data = stock_api.fetch_stock_data(ceo["marketSymbol"], date_from, date_to)
 
-        # TODO: remove - debugging purposes only
-        print(f"Stock data: {stock_data}")
+        logger.debug(f"Stock data: {stock_data}")
 
-        if stock_data is [] or stock_data is None:
+        if stock_data is None or stock_data == []:
+            logger.debug(f"No data found for the action within the specified time range: {date_from} - {date_to}")
             continue
 
         stock_30_m_before_tweet, stock_30_m_after_tweet = stock_data[-1], stock_data[0]
         correlation_results = analyze_correlation(sentiment, stock_30_m_after_tweet["last"], stock_30_m_before_tweet["last"])
 
-        # TODO: remove - debugging purposes only
-        print(f"Correlation results: {correlation_results}")
-        print(f"Stock 30 min before tweet: {stock_30_m_before_tweet}")
-        print(f"Stock 30 min after tweet: {stock_30_m_after_tweet}")
+        logger.debug(f"Stock data 30 min before tweet: {stock_30_m_before_tweet}")
+        logger.debug(f"Stock data 30 min after tweet: {stock_30_m_after_tweet}")
+        logger.debug(f"Correlation results: {correlation_results}")
 
-        data.append({
+        output_data.append({
             "accountName": ceo["twitterAccountName"],
             "tweet": tweet['text'],
             "date": tweet['created_at'],
@@ -74,7 +76,10 @@ for(i, tweet) in enumerate(tweets):
             "isColerationOccurs": correlation_results
         })
     except TimeFrameError as e:
+        logger.info(f"The timestamp of the specific tweet does not fall between 14:00 and 19:00. Moving on to the next tweet.")
         continue
+
+logger.info(f"Collected {len(output_data)} tweets with stock and sentiment analysis")
 
 export_methods = {
     "Export to csv": export_to_csv,
@@ -93,8 +98,9 @@ data_export_method = questionary.select(
         ]).ask()
 
 if data_export_method in export_methods:
-    export_methods[data_export_method](data, f"{ceo["name"]}_tweets_analysis")
+    file_name = f"{ceo["name"].lower().replace(" ", "_")}_tweets_analysis_{int(time.time())}"
+    export_methods[data_export_method](output_data, file_name)
     sys.exit(0)
 else:
-    print("Invalid export method")
+    logger.error("Invalid export method")
     sys.exit(1)
--- src/apis/twitter_api.py
+++ src/apis/twitter_api.py
@@ -1,9 +1,10 @@
 import json
-import tweepy
+import requests
 from typing import List
 from abc import ABC, abstractmethod
+from src.utils.logger import Logger
 from src.constants.main import MOCKS_DIRECTORY_PATH
-from src.interfaces.main import Tweet
+from src.interfaces.main import Tweet, Tweeter_User
 
 class AbstractTwitterApiStrategy(ABC):
     @abstractmethod
@@ -11,35 +12,83 @@ class AbstractTwitterApiStrategy(ABC):
         pass
 
 class TwitterMockApi(AbstractTwitterApiStrategy):
+    def __init__(self):
+        self.logger = Logger("Twitter Mock API Service")
+
     def _load_mock_data(self, username):
         try:
             self.user_tweets_mock_filepath = f'{MOCKS_DIRECTORY_PATH}/{username}_tweets_response_mock.json'
+            self.logger.debug(f"Loading mock data from {self.user_tweets_mock_filepath}")
+            
             with open(self.user_tweets_mock_filepath, 'r') as file:
                 return json.load(file)
         except FileNotFoundError:
-            print(f"Mock data file {self.user_tweets_mock_filepath} not found.")
+            self.logger.error(f"Mock data file {self.user_tweets_mock_filepath} not found.")
             return None
         except json.JSONDecodeError:
-            print(f"Error decoding JSON from file {self.user_tweets_mock_filepath}.")
+            self.logger.error(f"Error decoding JSON from file {self.user_tweets_mock_filepath}.")
             return None
 
     def fetch_user_tweets(self, username: str, count=10) -> List[Tweet] | None:
         mock_data = self._load_mock_data(username)
         if not mock_data:
             return None
-
+        
+        self.logger.debug(f"Mock data: {mock_data}")
         return [tweet for tweet in mock_data.get("includes", {}).get("tweets", [])][:count]
 
 class TwitterApi(AbstractTwitterApiStrategy):
-    def __init__(self, api_key, api_secret_key, access_token, access_token_secret):
-        auth = tweepy.OAuthHandler(api_key, api_secret_key)
-        auth.set_access_token(access_token, access_token_secret)
-        self.api = tweepy.API(auth)
+    def __init__(self, bearer_token):
+        self.logger = Logger("Twitter API Service")
+        self.base_url = "https://api.twitter.com/2/"
+        self.auth_headers = {
+            'authorization': f'Bearer {bearer_token}',
+        }
 
-    def fetchUserTweets(self, username, count=10) -> List[Tweet] | None:
+    def _get_request(self, endpoint, params=None):
+        self.logger.debug(f"Making request to {endpoint} with params: {params}")
+        return requests.get(f"{self.base_url}{endpoint}", headers=self.auth_headers, params=params)
+
+    def _get_user_by_username(self, username) -> Tweeter_User | None:
+        try: 
+            user_response = self._get_request(endpoint=f"users/by/username/{username}")
+            user_response_json = user_response.json()
+            self.logger.debug(f"User response status code: {user_response.status_code}")
+            self.logger.debug(f"User response JSON: {user_response_json}")
+
+            if "data" in user_response_json:
+                user_data: Tweeter_User = user_response_json["data"]
+                return user_data
+            else:
+                return None
+                
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"An error occurred while fetching user data: {e}")
+            return None
+
+    def fetch_user_tweets(self, username, count=10) -> List[Tweet] | None:
         try:
-            user_tweets = self.api.user_timeline(screen_name=username, count=count)
-            return [tweet for tweet in user_tweets.get("includes", {}).get("tweets", [])][:count]
-        except tweepy.HTTPException as e:
-            print(f"Error fetching tweets for {username} from Twitter API: {str(e)}")
+            user_data = self._get_user_by_username(username)
+            if user_data is None:
+                self.logger.error(f"User '{username}' not found.")
+                return None
+
+            user_id = user_data["id"]
+            user_tweets_response = self._get_request(endpoint=f"users/{user_id}/tweets", params={
+                "max_results": count,
+                "tweet.fields": "created_at,text",
+                "exclude": "retweets,replies"
+            })
+            user_tweets_json = user_tweets_response.json()
+            self.logger.debug(f"User tweets response status code: {user_tweets_response.status_code}")
+            self.logger.debug(f"User tweets response JSON: {user_tweets_json}")
+
+            if "data" in user_tweets_json:
+                user_tweets_data: List[Tweet] = user_tweets_json["data"]
+                return user_tweets_data
+            else:
+                return None
+            
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"An error occurred while fetching user tweets: {e}")
             return None
